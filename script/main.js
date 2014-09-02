5373878f0fcee7bfccd99cdb48cc8a824a4f166a
Avishay Lavie <avishay.lavie@gmail.com>
2014-09-02 18:55:33 +0300

    Add mouse controls

--- script/main.js
+++ script/main.js
@@ -12,6 +12,8 @@ define(['physics', 'surface'], function(physics, Surface) {
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 		camera.position.z = 100;
 		
+		var controls = new THREE.OrbitControls(camera);
+		
 		renderer = new THREE.WebGLRenderer();
 		document.body.appendChild(renderer.domElement);
 		
